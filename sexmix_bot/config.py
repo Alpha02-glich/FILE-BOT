da66f05e2e978360ea4a1a71c0b44aa5c73e4be2
Alpha02-glich <archanadixit232@gmail.com>
2025-08-04 12:00:50 +0530

    Update config.py

--- sexmix_bot/config.py
+++ sexmix_bot/config.py
@@ -1,9 +1,6 @@
-from dotenv import load_dotenv
 import os
 
-load_dotenv()
-
-API_ID = int(os.getenv("API_ID"))
-API_HASH = os.getenv("API_HASH")
-BOT_TOKEN = os.getenv("BOT_TOKEN")
-PROMO_LINK = os.getenv("PROMO_LINK")
+API_ID = int(os.getenv("API_ID", 0))
+API_HASH = os.getenv("API_HASH", "")
+BOT_TOKEN = os.getenv("BOT_TOKEN", "")
+PROMO_LINK = os.getenv("PROMO_LINK", "Join our channel for more!")
